cc68b23030fb7dfbda668e69a606b45a6da24f3b
dmendez1012 <dmendez@canvia.com>
2022-03-04 00:00:34 -0500

    se agregó el menú y el xml

---
+++ app/src/main/java/com/bim/utp/pe/mvvm/view/enviarDinero/EnviarDineroActivity.java
@@ -0,0 +1,16 @@
+package com.bim.utp.pe.mvvm.view.enviarDinero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bim.utp.pe.R;
+
+public class EnviarDineroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enviar_dinero);
+    }
+}
